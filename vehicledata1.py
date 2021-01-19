8540afe0c2022ea78cf774e493994b58e6205ac3
ssingh60 <ssingh60@g.syr.edu>
2021-01-19 01:21:13 -0500

    Vehicle Tracking Using Kafka

---
+++ vehicledata1.py
@@ -0,0 +1,38 @@
+from pykafka import KafkaClient
+import json
+from datetime import datetime
+import uuid
+import time
+
+input_file = open('./data/vehicle1.json')
+json_array = json.load(input_file)
+coordinates = json_array['features'][0]['geometry']['coordinates']
+
+def generate_uuid():
+    return uuid.uuid4()
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['vehicle_tracking']
+producer = topic.get_sync_producer()
+
+data = {}
+data['busline'] = '00001'
+
+def generate_checkpoint(coordinates):
+    i = 0
+    while i < len(coordinates):
+        data['key'] = data['busline'] + '_' + str(generate_uuid())
+        data['timestamp'] = str(datetime.utcnow())
+        data['latitude'] = coordinates[i][1]
+        data['longitude'] = coordinates[i][0]
+        message = json.dumps(data)
+        print(message)
+        producer.produce(message.encode('ascii'))
+        time.sleep(4)
+
+        if i == len(coordinates)-1:
+            i = 0
+        else:
+            i += 1
+
+generate_checkpoint(coordinates)
